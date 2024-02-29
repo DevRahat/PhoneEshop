112a8e0b89afe0ab4e55a1e688ce15ae89743652
DevRahat <dev.rahatislam@gmail.com>
2024-02-29 10:47:03 +0600

    2nd commit

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async() =>{
-    const res= await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async(searchText) =>{
+    const res= await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data= await res.json();
     const phones=data.data;
     console.log(phones);
@@ -8,10 +8,25 @@ const loadPhone = async() =>{
 }
 
 const displayPhones = phones =>{
+    
     //step 1 get the div we are going to addend 
     const phoneContainer=document.getElementById('phone-container');
+    //clear phone container cards before adding new search
+    phoneContainer.textContent ='';
+
+    //display see more button condition
+    const showAllContainer= document.getElementById('show-all-container');
+    if(phones.length >10){
+        showAllContainer.classList.remove('hidden')
+    }
+    else{
+        showAllContainer.classList.add('hidden')
+    }
+    //display Only  first 10 items
+    phones =phones.slice(0,10);
+    
     phones.forEach(phone =>{
-        console.log(phone);
+        
         //step 2 create a div 
         const phoneCard=document.createElement('div');
         phoneCard.classList = `card bg-gray-100 shadow-xl p-4`;
@@ -35,7 +50,8 @@ const handleSearch =() =>{
     const searchField=document.getElementById('search-field');
     const searchText=searchField.value;
     console.log(searchText);
+    loadPhone(searchText);
 
 }
 
-loadPhone();
+// loadPhone();
